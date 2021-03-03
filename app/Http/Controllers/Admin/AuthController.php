6486aa05bd3b2b47c78c67fcb6d5ab2bf353c2af
Hilton L Bacelar <hilton.bacelar@h9web.com.br>
2021-03-03 09:56:53 -0300

    Create AuthController

---
+++ app/Http/Controllers/Admin/AuthController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace LaraDev\Http\Controllers\Admin;
+
+use Illuminate\Http\Request;
+use LaraDev\Http\Controllers\Controller;
+
+class AuthController extends Controller
+{
+    public function showLoginForm() {
+        return view('admin.index');
+    }
+}
